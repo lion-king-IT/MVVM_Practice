9e21a1ef279b05b49b4dd63193aee6358ab61869
github lion-king-IT <reo.runningg@gmail.com>
2021-02-22 17:51:29 +0900

    LiveData実装

--- app/src/main/java/com/reo/running/myapplication/FootballFragment.kt
+++ app/src/main/java/com/reo/running/myapplication/FootballFragment.kt
@@ -4,14 +4,36 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import com.reo.running.myapplication.databinding.FragmentFootballBinding
 
 class FootballFragment : Fragment() {
+
+    private lateinit var binding: FragmentFootballBinding
+    private val viewModel = FootballViewModel()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_football, container, false)
+        binding = FragmentFootballBinding.inflate(inflater,container,false)
+        binding.vm = viewModel
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.editText.addTextChangedListener { text ->
+            viewModel.updateButton(text.isNullOrBlank())
+        }
+        binding.button.setOnClickListener {
+            val text = binding.editText.text.toString()
+            viewModel.submitText(text)
+        }
     }
 }
